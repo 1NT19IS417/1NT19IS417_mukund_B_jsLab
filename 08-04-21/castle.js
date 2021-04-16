8e0bc02783f98122c971dd70f1c190bb636b9d7e
Mukund.G <82136035+1NT19IS417@users.noreply.github.com>
2021-04-16 11:44:11 +0530

    Randomizing Div Colors

---
+++ 08-04-21/castle.js
@@ -0,0 +1,44 @@
+const b1 = document.getElementById("b1");
+const b2 = document.getElementById("b2");
+const b3 = document.getElementById("b3");
+const b4 = document.getElementById("b4");
+let flag = false;
+
+colors = ["red", "green", "blue", "black"];
+
+b1.addEventListener("mouseenter", () => {
+  console.log("Entered Road");
+});
+
+b2.addEventListener("mouseenter", () => {
+  console.log("Entered Temple Premises");
+});
+
+b3.addEventListener("mouseenter", () => {
+  console.log("Entered Passageway");
+});
+
+b4.addEventListener("mouseenter", () => {
+  console.log("Entered Sanctum , leave without clicking and die");
+});
+
+b4.addEventListener("click", () => {
+  flag = true;
+});
+
+function colorchange1() {
+  b2.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+}
+
+b2.addEventListener("click", () => {
+  colorchange1();
+});
+
+b4.addEventListener("mouseleave", () => {
+  if (!flag) {
+    console.log("Dead");
+  } else {
+    console.log("Alive");
+    flag = false;
+  }
+});
