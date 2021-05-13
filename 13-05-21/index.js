31b5945923969a79cff9feb797148fd4167b83d7
Mukund.G <82136035+1NT19IS417@users.noreply.github.com>
2021-05-13 14:50:01 +0530

    JS Updated

---
+++ 13-05-21/index.js
@@ -0,0 +1,8 @@
+// Clears the screen on click of C button.
+function clearVal() {
+  document.getElementById("result").value = "";
+}
+// Displays entered value on screen.
+function displayVal(value) {
+  document.getElementById("result").value += value;
+}
